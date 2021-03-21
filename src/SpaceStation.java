86e5eaa8447e4562db7f5cdb963f5183baf550d6
AlvaroP <alvaro17pc@gmail.com>
2021-03-21 13:30:49 +0100

    Falta añadir las armas y los sitemas de defensa en todas, y despues terminar la estacion espacial y el hangar

---
+++ src/SpaceStation.java
@@ -0,0 +1,25 @@
+public class SpaceStation extends Ship {
+
+    private final static int MaxPassengers = 1000;
+    //private SistemaDefensa (3);
+
+    private class Hangar {
+
+        //private ArrayNaves;
+
+        public void AddShip() {
+
+        }
+
+        public void RemoveShip() {
+
+        }
+
+
+        public Hangar() {
+        }
+    }
+
+    public SpaceStation() {
+    }
+}
