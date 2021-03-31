06b52eff0d08722ad3dce49324d75ea909feb9be
AlvaroP <alvaro17pc@gmail.com>
2021-03-31 19:03:31 +0200

    añado las operaciones del array de barcos (no se si estan bien)

--- src/SpaceStation.java
+++ src/SpaceStation.java
@@ -1,10 +1,36 @@
+import java.util.ArrayList;
+
 public class SpaceStation extends Ship {
 
     private final static int MaxPassengers = 1000;
     //private SistemaDefensa (3);
-    //private Ship (n);
+    private ArrayList<Ship> list = new ArrayList<>();
 
 
-    public SpaceStation() {
+    public static int getMaxPassengers() {
+        return MaxPassengers;
+    }
+
+    public ArrayList<Ship> getShips() {
+        return list;
+    }
+
+    public void setListShips(ArrayList<Ship> list) {
+        this.list = list;
+    }
+
+    public void addShip (Ship S) {
+
+        if (!list.contains(S)) {
+            list.add(S);
+        }
+    }
+
+    public void removeShip (Ship S) {
+        list.remove(S);
+    }
+
+    public SpaceStation(ArrayList<Ship> list) {
+        this.list = list;
     }
 }
