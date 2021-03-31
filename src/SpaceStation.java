e94c230ea1ad3f3555629cd533fb929caaeb5b96
AlvaroP <alvaro17pc@gmail.com>
2021-03-31 18:30:23 +0200

    cambio lo del hangar por un ship

--- src/SpaceStation.java
+++ src/SpaceStation.java
@@ -2,23 +2,8 @@ public class SpaceStation extends Ship {
 
     private final static int MaxPassengers = 1000;
     //private SistemaDefensa (3);
+    //private Ship (n);
 
-    private class Hangar {
-
-        //private ArrayNaves;
-
-        public void AddShip() {
-
-        }
-
-        public void RemoveShip() {
-
-        }
-
-
-        public Hangar() {
-        }
-    }
 
     public SpaceStation() {
     }
