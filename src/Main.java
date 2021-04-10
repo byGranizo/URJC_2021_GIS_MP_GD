c2aa6faa65e86b3e81bde0a595dc76765561c95c
Antonio <Antoniopajueloc21@gmail.com>
2021-04-10 14:21:39 +0200

    Create Register Client and login

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,89 @@
+import Ships.Ship;
+import bd.Database;
 import bd.Systems;
+import models.Customers;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hola");
+        Systems system = new Systems();
+        int option = 0;
+        if(Database.existsData()){
+            system = Database.loadData();
+        }
+        while(option != 3){
+            option = init();
+            switch (option){
+                case 1 :
+                    registerCustomer(system);
+                    break;
+                case 2 :
+                    Boolean login = login(system);
+                    if(login){
+                        int optionSystem = 0;
+                        while (optionSystem != 5){
+                            optionSystem = menuSystem();
+                        }
+
+                    }
+                    break;
+                case 3 :
+                    break;
+                default:
+                    System.out.println("invalid option");
+            }
+        }
     }
+
+    private static boolean login(Systems system) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Username: ");
+        String username = scan.nextLine();
+        System.out.println("Password: ");
+        String password = scan.nextLine();
+        Boolean login = system.LogIn(username, password);
+        return login;
+    }
+
+    private static void registerCustomer(Systems system) {
+        Scanner scan = new Scanner(System.in);
+        List<Ship> ships = new ArrayList<>();
+        System.out.println("Nick: ");
+        String nick = scan.nextLine();
+        System.out.println("Password: ");
+        String pass = scan.nextLine();
+        System.out.println("Name: ");
+        String name = scan.nextLine();
+        System.out.println("Email: ");
+        String email = scan.nextLine();
+        System.out.println("Origin: ");
+        String origin = scan.nextLine();
+        System.out.println("Spices: ");
+        String spices = scan.nextLine();
+        String id = (""+system.getListCostumers().size());
+        Customers customers = new Customers(name,origin,spices,id,nick,pass,email,0,false,false,false);
+        system.getListCostumers().add(customers);
+    }
+
+    private static int menuSystem(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("1. Register Client");
+        System.out.println("1. Register Client");
+        System.out.println("1. Register Client");
+        System.out.println("1. Register Client");
+        System.out.println("1. Register Client");
+        return scan.nextInt();
+    }
+
+    private static int init(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("1. Register Client");
+        System.out.println("2. Login");
+        System.out.println("3. Exit");
+        return scan.nextInt();
+    }
+
 }
--- src/bd/Systems.java
+++ src/bd/Systems.java
@@ -14,8 +14,15 @@ public class Systems {
     private List<Offer> ListOffer = new ArrayList<Offer>();
 
 
-    public void LogIn(String usarname, String pasword) {
-
+    public boolean LogIn(String username, String pasword) {
+        boolean login = false;
+        for (Customers customer: listCostumers) {
+            if(customer.getNick().equals(username) && customer.getPassword().equals(pasword)){
+                login = true;
+                break;
+            }
+        }
+        return login;
     }
 
     public void AddOffer(Object Offer) {
@@ -35,6 +42,30 @@ public class Systems {
     }
     public void UploadComments(Object customer){
 
+    }
+
+    public List<Customers> getListCostumers() {
+        return listCostumers;
+    }
+
+    public void setListCostumers(List<Customers> listCostumers) {
+        this.listCostumers = listCostumers;
+    }
+
+    public List<Admin> getListAdmins() {
+        return ListAdmins;
+    }
+
+    public void setListAdmins(List<Admin> listAdmins) {
+        ListAdmins = listAdmins;
+    }
+
+    public List<Offer> getListOffer() {
+        return ListOffer;
+    }
+
+    public void setListOffer(List<Offer> listOffer) {
+        ListOffer = listOffer;
     }
 }
 
